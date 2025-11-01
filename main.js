89394a5961907078223bf0151ade8c5528b06862
fetrainee <230843689+fetrainee@users.noreply.github.com>
2025-11-01 19:10:11 +0100

    Add a Success message section

--- main.js
+++ main.js
@@ -3,9 +3,12 @@ const input = document.querySelector('#email');
 const errorLabel = document.querySelector('.email-form__error');
 
 button.addEventListener('click', () => {
-    if (input.checkValidity()) {
+    if (input.checkValidity() && input.value !== '') {
         errorLabel.textContent = '';
         input.classList.remove('email-form__input--error');
+
+        const validEmail = input.value;
+        document.querySelector('.success-message__description').innerHTML = `A confirmation email has been sent to <b>${validEmail}</b>. Please open it and click the button inside to confirm your subscription`
     } else {
         errorLabel.textContent = 'Valid email required';
         input.classList.add('email-form__input--error');
