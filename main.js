4afd5d9710dca813968ba1f368ec16699ae9e4ea
fetrainee <230843689+fetrainee@users.noreply.github.com>
2025-11-01 19:20:47 +0100

    Add sections visibility toggle

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
-const button = document.querySelector('.email-form__button');
+const subscribeButton = document.querySelector('.email-form__button');
+const dismissButton = document.querySelector('.success-message__button');
 const input = document.querySelector('#email');
 const errorLabel = document.querySelector('.email-form__error');
 
-button.addEventListener('click', () => {
+subscribeButton.addEventListener('click', () => {
     if (input.checkValidity() && input.value !== '') {
         errorLabel.textContent = '';
         input.classList.remove('email-form__input--error');
 
         const validEmail = input.value;
         document.querySelector('.success-message__description').innerHTML = `A confirmation email has been sent to <b>${validEmail}</b>. Please open it and click the button inside to confirm your subscription`
+
+        document.querySelector('.success-message').classList.remove('success-message__hidden');
+        document.querySelector('.newsletter-signup').classList.add('newsletter-signup__hidden');
+
     } else {
         errorLabel.textContent = 'Valid email required';
         input.classList.add('email-form__input--error');
     }
+});
+
+dismissButton.addEventListener('click', () => {
+    document.querySelector('.success-message').classList.add('success-message__hidden');
+    document.querySelector('.newsletter-signup').classList.remove('newsletter-signup__hidden');
 });
 
 input.addEventListener('input', () => {
