8cb082a786bd95408f56c86b176c91673e0e6b2e
fetrainee <230843689+fetrainee@users.noreply.github.com>
2025-11-01 17:24:33 +0100

    BEM refactoring

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-const button = document.querySelector('.form--button');
+const button = document.querySelector('.email-form__button');
 const input = document.querySelector('#email');
-const errorLabel = document.querySelector('.label-error');
+const errorLabel = document.querySelector('.email-form__error');
 
 button.addEventListener('click', () => {
     if (input.checkValidity()) {
         errorLabel.textContent = '';
-        input.classList.remove('email-input__error');
+        input.classList.remove('email-form__input--error');
     } else {
         errorLabel.textContent = 'Valid email required';
-        input.classList.add('email-input__error');
+        input.classList.add('email-form__input--error');
     }
 });
